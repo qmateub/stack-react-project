a40ff0efbc17e577c81b6d71c1f040439718e3f4
qmateub <qmateub@yahoo.es>
2017-12-25 20:21:52 +0100

    feat(app): first commit
    first commit

---
+++ src/app/components/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Main extends React.Component {
+  static displayName = 'Main';
+
+  render() {
+    return <div>TEST1</div>;
+  }
+}
+
+export default Main;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var DIST_DIR = path.resolve(__dirname, 'dist');
+var SRC_DIR = path.resolve(__dirname, 'src');
+
+module.exports = {
+  entry: SRC_DIR + '/app/index.js',
+  output: {
+    path: DIST_DIR + '/app',
+    filename: 'bundle.js',
+    publicPath: '/app/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'stage-0'],
+        },
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+    ],
+  },
+};
